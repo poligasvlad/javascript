7ab0585a9d8de598246360fb2d219fc4b8369fde
poligasvlad <poligasvlad@gmail.com>
2022-01-24 15:23:26 +0200

    slider

--- js/script.js
+++ js/script.js
@@ -1,121 +1,169 @@
-document.addEventListener('DOMContentLoaded', () =>{
+document.addEventListener('DOMContentLoaded', () => {
     //Tabs
     const tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
         tabsParent = document.querySelector('.tabheader__items');
 
-        function hideTabContent(){
-            tabsContent.forEach(item =>{
-                item.style.display = 'none';
-            })
-
-            tabs.forEach(item => {
-                item.classList.remove('tabheader__item_active');
-            })
-        }
-
-        function showTabContent(i = 0){
-            tabsContent[i].style.display = 'block';
-            tabs[i].classList.add('tabheader__item_active')
-        }
-
-        hideTabContent();
-        showTabContent();
-
-        tabsParent.addEventListener('click', (event) =>{
-            const target = event.target;
-
-            if (target && target.classList.contains('tabheader__item')) {
-                tabs.forEach((item, i) => {
-                    if (target == item) {
-                        hideTabContent();
-                        showTabContent(i);  
-                    }
-                });
-            }
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.style.display = 'none';
         })
-        //Time
-        const deadline = "2022-01-22";
 
-        function getTimeRemaining(endtime) {
-            const t = Date.parse(endtime) - Date.parse(new Date()),
-                  days = Math.floor(t / (1000 * 60 * 60 *24)),
-                  hours = Math.floor((t / (1000 * 60 * 60) % 24)),
-                  minutes = Math.floor((t / (1000 * 60 ) % 60)),
-                  seconds = Math.floor((t / 1000 % 60));
-                  return{
-                        'total': t,
-                        'days': days,
-                        'hours': hours,
-                        'minutes': minutes,
-                        'seconds': seconds
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        })
+    }
 
-                  }
+    function showTabContent(i = 0) {
+        tabsContent[i].style.display = 'block';
+        tabs[i].classList.add('tabheader__item_active')
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
         }
+    })
+    //Time
+    const deadline = "2022-03-03";
 
-        function getZero(num){
-            if(num >= 0 && num < 10){
-                return `0${num}`; 
-            } else{
-                return num;
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+            minutes = Math.floor((t / (1000 * 60) % 60)),
+            seconds = Math.floor((t / 1000 % 60));
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+
+        }
+    }
+
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
             }
         }
+    }
 
-        function setClock (selector,endtime){
-            const timer = document.querySelector(selector),
-                  days = timer.querySelector('#days'),
-                  hours = timer.querySelector('#hours'),
-                  minutes = timer.querySelector('#minutes'),
-                  seconds = timer.querySelector('#seconds'),
-                  timeInterval = setInterval(updateClock, 1000);
-                
-                  updateClock();
-
-          function updateClock(){
-              const t = getTimeRemaining(endtime);
-
-              days.innerHTML = getZero(t.days);
-              hours.innerHTML = getZero(t.hours);
-              minutes.innerHTML = getZero(t.minutes);
-              seconds.innerHTML = getZero(t.seconds);
-
-              if (t.total <= 0){
-                  clearInterval(timeInterval);
-              }
-          }
-        }
-        setClock('.timer', deadline);
+    setClock('.timer', deadline);
 
 
 //Modal
 
     const modalTrigger = document.querySelectorAll('button[data-modal]')
     const modal = document.querySelector('.modal')
-    const modalCloseBtn = document.querySelector ('[data-close]');
+    const modalCloseBtn = document.querySelector('[data-close]');
 
-      modalTrigger.forEach (btn => {
-          btn.addEventListener('click', () =>{
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', () => {
             modal.classList.add('show');
             document.body.style.overflow = 'hidden';
-          })
-      })
+        })
+    })
 
-      function closeModal(){ 
-        modal.classList.remove('show')   
+    function closeModal() {
+        modal.classList.remove('show')
         document.body.style.overflow = 'auto';
-      }
-      modalCloseBtn.addEventListener('click', closeModal);
+    }
+
+    modalCloseBtn.addEventListener('click', closeModal);
 
 
-      modal.addEventListener('click', (event) => {
-         if(event.target === modal){
-             closeModal();
-         }
-      })
+    modal.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            closeModal();
+        }
+    })
 
-      document.addEventListener('keydown', (event) =>{
-          if ( event.code === "Escape"){
-              closeModal();
-          }
-      })
+    document.addEventListener('keydown', (event) => {
+        if (event.code === "Escape") {
+            closeModal();
+        }
+    })
+
+
+//Slider
+    let sliderImg = document.querySelectorAll('.offer__slide');
+    let currentNum = document.getElementById('current');
+    const nextSlide = document.querySelector('.offer__slider-next');
+    const prevSlide = document.querySelector('.offer__slider-prev');
+    let slideIndex = 2;
+    sliderImg[2].classList.add('show');
+
+        nextSlide.addEventListener('click', function (){
+            if (slideIndex < 3){
+                slideIndex ++;
+
+            }else{
+                slideIndex = 0
+                sliderImg[3].classList.remove('show');
+            }
+            updateNum();
+            showImg();
+            sliderImg[slideIndex - 1].classList.remove('show');
+
+        });
+        prevSlide.addEventListener('click',function (){
+            if (slideIndex > 0){
+                slideIndex --;
+
+            }else{
+                slideIndex = 3;
+                sliderImg[0].classList.remove('show');
+            }
+
+            updateNum();
+            showImg();
+            sliderImg[slideIndex + 1].classList.remove('show');
+        });
+
+        function updateNum(){
+            currentNum.innerHTML = getZero(slideIndex + 1);
+        };
+        function showImg(){
+            console.log(slideIndex)
+            sliderImg[slideIndex].classList.add('show');
+            console.log(sliderImg);
+        };
+
 })
